4af03c65745e20b85067acaeeb26325a543d6039
Sam Bilbow <31018239+sambilbow@users.noreply.github.com>
2022-04-18 15:32:27 +0100

    Update sentinel_self_arduino_ethernet.ino
    Add unique MAC, and static IPs

--- sentinel_self_arduino_ethernet/sentinel_self_arduino_ethernet.ino
+++ sentinel_self_arduino_ethernet/sentinel_self_arduino_ethernet.ino
@@ -1,64 +1,42 @@
-/*
- Web client
-
- This sketch connects to a website (http://www.google.com)
- using an Arduino WIZnet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe, based on work by Adrian McEwen
-
- */
-
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// Choose the MAC address (check box ID)
+// byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEC, 0xF8 }; // Heart Rate System 1
+// byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEE, 0xD5 }; // Heart Rate System 2
+// byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEC, 0x9B }; // Heart Rate System 3
+ byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEE, 0xD9 }; // Heart Rate System 4
 
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "wifitest.adafruit.com";    // name address for adafruit (using DNS)
+// Set the static IP address to use (check box ID)
+// byte ip[] = {192, 168, 0, 201}; // Heart Rate System 1
+// byte ip[] = {192, 168, 0, 202}; // Heart Rate System 2
+// byte ip[] = {192, 168, 0, 203}; // Heart Rate System 3
+ byte ip[] = {192, 168, 0, 204}; // Heart Rate System 4
 
-// Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192, 168, 0, 177);
-IPAddress myDns(192, 168, 0, 1);
+// Set the router gateway IP and subnet
+byte gateway[] = { 192, 168, 0, 1 };
+byte subnet[] = { 255, 255, 255, 0 }; 
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-EthernetClient client;
-
-// Variables to measure the speed
-unsigned long beginMicros, endMicros;
-unsigned long byteCount = 0;
-bool printWebData = true;  // set to false for better speed measurement
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
 
 void setup() {
   // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH Shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit FeatherWing Ethernet
-  Ethernet.init(33);  // ESP32 with Adafruit FeatherWing Ethernet
+  Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
 
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  Serial.println("Ethernet WebServer Example");
 
-  // start the Ethernet connection:
-  Serial.println("Initialize Ethernet with DHCP:");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
+
+    // start the Ethernet connection and the server:
+    Ethernet.begin(mac, ip, gateway, subnet);
+
     // Check for Ethernet hardware present
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
       Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
@@ -69,68 +47,64 @@ void setup() {
     if (Ethernet.linkStatus() == LinkOFF) {
       Serial.println("Ethernet cable is not connected.");
     }
-    // try to configure using IP address instead of DHCP:
-    Ethernet.begin(mac, ip, myDns);
-  } else {
-    Serial.print("  DHCP assigned IP ");
+
+    // start the server
+    server.begin();
+    Serial.print("server is at ");
     Serial.println(Ethernet.localIP());
   }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.print("connecting to ");
-  Serial.print(server);
-  Serial.println("...");
 
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.print("connected to ");
-    Serial.println(client.remoteIP());
-    // Make a HTTP request:
-    client.println("GET /testwifi/index.html HTTP/1.1");
-    client.println("Host: wifitest.adafruit.com");
-    client.println("Connection: close");
-    client.println();
-  } else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
-  beginMicros = micros();
-}
 
-void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  int len = client.available();
-  if (len > 0) {
-    byte buffer[80];
-    if (len > 80) len = 80;
-    client.read(buffer, len);
-    if (printWebData) {
-      Serial.write(buffer, len); // show in the serial monitor (slows some boards)
-    }
-    byteCount = byteCount + len;
-  }
+  void loop() {
+    // listen for incoming clients
+    EthernetClient client = server.available();
+    if (client) {
+      Serial.println("new client");
+      // an http request ends with a blank line
+      boolean currentLineIsBlank = true;
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
+          Serial.write(c);
+          // if you've gotten to the end of the line (received a newline
+          // character) and the line is blank, the http request has ended,
+          // so you can send a reply
+          if (c == '\n' && currentLineIsBlank) {
+            // send a standard http response header
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");  // the connection will be closed after completion of the response
+            client.println("Refresh: 1");  // refresh the page automatically every 5 sec
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html>");
+            // output the value of each analog input pin
+            for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+              int sensorReading = analogRead(analogChannel);
+              client.print("analog input ");
+              client.print(analogChannel);
+              client.print(" is ");
+              client.print(sensorReading);
+              client.println("<br />");
+            }
+            client.println("</html>");
+            break;
+          }
+          if (c == '\n') {
+            // you're starting a new line
+            currentLineIsBlank = true;
+          } else if (c != '\r') {
+            // you've gotten a character on the current line
+            currentLineIsBlank = false;
+          }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    endMicros = micros();
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    Serial.print("Received ");
-    Serial.print(byteCount);
-    Serial.print(" bytes in ");
-    float seconds = (float)(endMicros - beginMicros) / 1000000.0;
-    Serial.print(seconds, 4);
-    float rate = (float)byteCount / seconds / 1000.0;
-    Serial.print(", rate = ");
-    Serial.print(rate);
-    Serial.print(" kbytes/second");
-    Serial.println();
 
-    // do nothing forevermore:
-    while (true) {
+        }
+      }
+      // give the web browser time to receive the data
       delay(1);
+      // close the connection:
+      client.stop();
+      Serial.println("client disconnected");
     }
   }
-}
