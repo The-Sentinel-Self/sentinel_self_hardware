b9d26e44f402ba7bb01b44a52417e4f19642e95c
Sam Bilbow <31018239+sambilbow@users.noreply.github.com>
2022-04-19 11:23:26 +0100

    Update sentinel_self_arduino_ethernet.ino
    Added OSC networking to the sketch

--- sentinel_self_arduino_ethernet/sentinel_self_arduino_ethernet.ino
+++ sentinel_self_arduino_ethernet/sentinel_self_arduino_ethernet.ino
@@ -1,110 +1,136 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <OSCMessage.h>
+#define USE_ARDUINO_INTERRUPTS false
+#include <PulseSensorPlayground.h>
+EthernetUDP Udp;
+PulseSensorPlayground pulseSensor;
 
 // Choose the MAC address (check box ID)
 // byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEC, 0xF8 }; // Heart Rate System 1
 // byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEE, 0xD5 }; // Heart Rate System 2
 // byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEC, 0x9B }; // Heart Rate System 3
- byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEE, 0xD9 }; // Heart Rate System 4
+byte mac[] = { 0x98, 0x76, 0xB6, 0x11, 0xEE, 0xD9 }; // Heart Rate System 4
 
 // Set the static IP address to use (check box ID)
 // byte ip[] = {192, 168, 0, 201}; // Heart Rate System 1
 // byte ip[] = {192, 168, 0, 202}; // Heart Rate System 2
 // byte ip[] = {192, 168, 0, 203}; // Heart Rate System 3
- byte ip[] = {192, 168, 0, 204}; // Heart Rate System 4
+byte ip[] = {192, 168, 0, 204}; // Heart Rate System 4
+
+// Receiver
+byte outIp[] = {192, 168, 0, 18}; // PC Static IP
+const unsigned int outPort = 7539; // PC Static IP Receive Port
 
 // Set the router gateway IP and subnet
 byte gateway[] = { 192, 168, 0, 1 };
-byte subnet[] = { 255, 255, 255, 0 }; 
+byte subnet[] = { 255, 255, 255, 0 };
 
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
-EthernetServer server(80);
+
+
+
+// Heart Rate Variables
+const int OUTPUT_TYPE = SERIAL_PLOTTER;
+const int PULSE_INPUT = 26;    // A0's GPIO
+const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
+const int PULSE_FADE = 5;
+const int THRESHOLD = 1900;   // Adjust this number to avoid noise when idle
+byte samplesUntilReport;
+const byte SAMPLES_PER_SERIAL_SAMPLE = 10;
+
+
+
+
+
 
 void setup() {
+  
   // You can use Ethernet.init(pin) to configure the CS pin
   Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
-
+  
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.println("Ethernet WebServer Example");
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip, gateway, subnet);
 
-
-    // start the Ethernet connection and the server:
-    Ethernet.begin(mac, ip, gateway, subnet);
-
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      while (true) {
-        delay(1); // do nothing, no point running without Ethernet hardware
-      }
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-
-    // start the server
-    server.begin();
-    Serial.print("server is at ");
-    Serial.println(Ethernet.localIP());
-  }
-
-
-  void loop() {
-    // listen for incoming clients
-    EthernetClient client = server.available();
-    if (client) {
-      Serial.println("new client");
-      // an http request ends with a blank line
-      boolean currentLineIsBlank = true;
-      while (client.connected()) {
-        if (client.available()) {
-          char c = client.read();
-          Serial.write(c);
-          // if you've gotten to the end of the line (received a newline
-          // character) and the line is blank, the http request has ended,
-          // so you can send a reply
-          if (c == '\n' && currentLineIsBlank) {
-            // send a standard http response header
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: text/html");
-            client.println("Connection: close");  // the connection will be closed after completion of the response
-            client.println("Refresh: 1");  // refresh the page automatically every 5 sec
-            client.println();
-            client.println("<!DOCTYPE HTML>");
-            client.println("<html>");
-            // output the value of each analog input pin
-            for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-              int sensorReading = analogRead(analogChannel);
-              client.print("analog input ");
-              client.print(analogChannel);
-              client.print(" is ");
-              client.print(sensorReading);
-              client.println("<br />");
-            }
-            client.println("</html>");
-            break;
-          }
-          if (c == '\n') {
-            // you're starting a new line
-            currentLineIsBlank = true;
-          } else if (c != '\r') {
-            // you've gotten a character on the current line
-            currentLineIsBlank = false;
-          }
-
-
-        }
-      }
-      // give the web browser time to receive the data
-      delay(1);
-      // close the connection:
-      client.stop();
-      Serial.println("client disconnected");
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
     }
   }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+  Udp.begin(8888);
+  // Print IP details
+  Serial.print("Sentinel is at ");
+  Serial.println(Ethernet.localIP());
+
+
+
+  
+  // Configure the PulseSensor manager.
+  pulseSensor.analogInput(PULSE_INPUT);
+  pulseSensor.blinkOnPulse(PULSE_BLINK);
+  pulseSensor.fadeOnPulse(PULSE_FADE);
+  pulseSensor.setSerial(Serial);
+  pulseSensor.setOutputType(OUTPUT_TYPE);
+  pulseSensor.setThreshold(THRESHOLD);
+  
+  // Skip the first SAMPLES_PER_SERIAL_SAMPLE in the loop().
+  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+  
+  // Now that everything is ready, start reading the PulseSensor signal.
+  if (!pulseSensor.begin()) {
+    for (;;) {
+      // Flash the led to show things didn't work.
+      digitalWrite(PULSE_BLINK, LOW);
+      delay(50);
+      digitalWrite(PULSE_BLINK, HIGH);
+      delay(50);
+    }
+  }
+
+}
+
+
+void loop() {
+  if (pulseSensor.sawNewSample()) {
+    if (--samplesUntilReport == (byte) 0) {
+      samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+      pulseSensor.outputSample();
+      if (pulseSensor.sawStartOfBeat()) {
+        pulseSensor.outputBeat();
+      }
+        OSCMessage rate("/sentinel/4/rate");
+        rate.add(pulseSensor.getBeatsPerMinute());
+        // Serial.println(pulseSensor.getBeatsPerMinute());
+        Udp.beginPacket(outIp, outPort);
+        rate.send(Udp); // send the bytes to the SLIP stream
+        Udp.endPacket(); // mark the end of the OSC Packet
+        rate.empty(); // free space occupied by message
+
+        OSCMessage ibi("/sentinel/4/ibi");
+        ibi.add(pulseSensor.getInterBeatIntervalMs());
+        // Serial.println(pulseSensor.getInterBeatIntervalMs());
+        Udp.beginPacket(outIp, outPort);
+        ibi.send(Udp); // send the bytes to the SLIP stream
+        Udp.endPacket(); // mark the end of the OSC Packet
+        ibi.empty(); // free space occupied by message
+        
+        OSCMessage pulse("/sentinel/4/pulse");
+        pulse.add(pulseSensor.getLatestSample());
+        // Serial.println(pulseSensor.getLatestSample());
+        Udp.beginPacket(outIp, outPort);
+        pulse.send(Udp); // send the bytes to the SLIP stream
+        Udp.endPacket(); // mark the end of the OSC Packet
+        pulse.empty(); // free space occupied by message
+    }
+  }
+}
